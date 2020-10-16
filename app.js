9e72a9b55f6302c3d83e21ca3dfdef548b851370
Daniel Bodmer <danny.bodmer@gmail.com>
2020-10-16 09:46:14 +0100

    update app.js

--- app.js
+++ app.js
@@ -8,5 +8,45 @@ const btnMul = document.querySelector('#btnMul');
 const btnDiv = document.querySelector('#btnDiv');
 const res = document.querySelector('#res');
 
-const clear = () => (res.value = null);
+const sym = ['-', '+', '*', '/'];
+const vals = [];
+let binStr = '';
+let answer = 0;
+
+const clear = () => {
+  res.value = null;
+  vals = [];
+};
+
+const updateScreen = (val) => () => {
+  switch (val) {
+    case '-':
+    case '+':
+    case '*':
+    case '/':
+      if (res.value && !sym.includes(res.value[res.value.length - 1])) {
+        vals.push(parseInt(binStr, 2));
+        binStr = '';
+        res.value += val;
+      }
+      break;
+    default:
+      res.value += val;
+      binStr += val;
+  }
+  console.log(vals);
+};
+
+const equal = () => {
+  res.value = answer;
+};
+
 btnClr.onclick = clear;
+btnEql.onclick = equal;
+
+btn0.onclick = updateScreen('0');
+btn1.onclick = updateScreen('1');
+btnSum.onclick = updateScreen('+');
+btnSub.onclick = updateScreen('-');
+btnMul.onclick = updateScreen('*');
+btnDiv.onclick = updateScreen('/');
